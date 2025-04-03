d573339b40a42c36f2b0a0fefdd228348dad3365
gayv4468 <gayv4468@gmail.com>
2025-04-03 13:10:33 +0000

    novo commit css e terceiro do js

--- script.js
+++ script.js
@@ -30,11 +30,19 @@ pessoas Number (evento.target.value)
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 ✓ botoesGorjeta.forEach(botao => {
-botao.addEventListener("click", receber Porcentagem)
+botao.addEventListener("click", receberPorcentagemBotao)
 })
 
-✓ function receber Porcentagem (evento) {
-console.log(evento.target.value)
-// porcentagem = Number(evento.target.value) / 180
-// console.log(porcentagem)
+
+function receberPorcentagemBotao (evento) {
+    botoesGorjeta.forEach(botao => {
+    botao.classList.remove("botao-ativo")
+
+    if(botao.value === evento.target.value) {
+    botao.classList.add("botao-ativo")
+    }
+    })
+
+    porcentagem = parseFloat(evento.target.value) / 100
+    }
 }
