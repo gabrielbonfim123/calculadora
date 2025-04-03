a1132ea7052f440736fb1dfb33bc93ee46361a86
gayv4468 <gayv4468@gmail.com>
2025-04-03 13:47:09 +0000

    sexto commit do js e agora sim finalizado

--- script.js
+++ script.js
@@ -88,4 +88,9 @@ function receberPorcentagem (evento) {
     document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
     document.querySelector(".total > strong").innerHTML = "R$ 0.00"
 
+
+conta = 0
+porcentagem = 0
+pessoas = 0
+
     }
